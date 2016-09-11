fe5da61cd9837f3e3d382b796bafdd3d210b7674
SylvainMarty <sylvain.marty@aol.fr>
2016-09-11 20:35:12 +0200

    Sectored loading dependencies

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 require.config({
     baseUrl: '/app',
-    urlArgs: 'v=1.0'
+    urlArgs: 'v=1.0',
+    map: {
+        '*': {
+            'css': 'node_modules/require-css/css.min.js' // or whatever the path to require-css is
+        }
+    }
 });
+
 
 require(
     [
-        // Add your angular components here
+        // Stylesheets (begin by css!/)
+        'css!/static/css/style.css',
+
+        // JavaScript (add .js if files are in node_modules)
         'app',
         'config',
         'routes',
         'services/routeResolver',
-        'services/properties'
+        'services/properties',
+        'node_modules/angular-route/angular-route.min.js'
+
+        // Add your angular components here
     ],
     function () {
         angular.bootstrap(document, ['webApp']);
